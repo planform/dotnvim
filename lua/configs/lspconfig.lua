b505d0157cfb33527ff93ec5c85119688a37415e
planform <steveli421@gmail.com>
2026-06-30 15:52:22 +0800

    Round LSP floating window borders

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local rounded_float = { border = "rounded" }
+local default_hover = vim.lsp.buf.hover
+local default_signature_help = vim.lsp.buf.signature_help
+
+vim.lsp.buf.hover = function(config)
+    return default_hover(vim.tbl_deep_extend("force", config or {}, rounded_float))
+end
+
+vim.lsp.buf.signature_help = function(config)
+    return default_signature_help(vim.tbl_deep_extend("force", config or {}, rounded_float))
+end
+
 local servers = { "html", "cssls", "rust_analyzer" }
 vim.lsp.enable(servers)
 
